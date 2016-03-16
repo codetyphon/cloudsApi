d6089282fe1c8918299e1352e9bd6df996078518
codetyphon <codetyphon@sina.com>
2016-03-16 17:29:32 +0800

    3

--- public/ctrl/ctrl.js
+++ public/ctrl/ctrl.js
@@ -1,23 +1,28 @@
 $(document).ready(function(){
     $('.switch').bind('click',function(){
         console.log($(this).attr('switch'));
-        switch ($(this).attr('switch')){
+        var $btn=$(this);
+        switch ($btn.attr('switch')){
             case 'led_on':
+                $btn.text('正在打开');
                 $.get('/led/on',function(){
                     get_led_status();
                 });
                 break;
             case 'led_off':
+                $btn.text('正在关闭');
                 $.get('/led/off',function(){
                     get_led_status();
                 });
                 break;
             case 'lamp_on':
+                $btn.text('正在打开');
                 $.get('/lamp/on',function(){
                     get_lamp_status();
                 });
                 break;
             case 'lamp_off':
+                $btn.text('正在关闭');
                 $.get('/lamp/off',function(){
                     get_lamp_status();
                 });
@@ -34,10 +39,12 @@ $(document).ready(function(){
             if(result==="status=on"){
                 console.log('on');
                 $('.switch[switch=led_on]').attr('class','switch weui_btn weui_btn_primary');
+                $('.switch[switch=led_on]').text('led on');
             }
             if(result==="status=off"){
                 console.log('off');
                 $('.switch[switch=led_off]').attr('class','switch weui_btn weui_btn_primary');
+                $('.switch[switch=led_on]').text('led off');
 
             }
         });
@@ -51,11 +58,12 @@ $(document).ready(function(){
             if(result==="status=on"){
                 console.log('on');
                 $('.switch[switch=lamp_on]').attr('class','switch weui_btn weui_btn_primary');
+                $('.switch[switch=lamp_on]').text('led on');
             }
             if(result==="status=off"){
                 console.log('off');
                 $('.switch[switch=lamp_off]').attr('class','switch weui_btn weui_btn_primary');
-
+                $('.switch[switch=lamp_on]').text('led off');
             }
         });
     }
