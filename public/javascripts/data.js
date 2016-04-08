4ed3278d9cf1e7441c54631f2730fcfff359ad48
codetyphon <codetyphon@sina.com>
2016-04-08 15:19:15 +0800

    ...

--- public/javascripts/data.js
+++ public/javascripts/data.js
@@ -5,6 +5,8 @@ $(document).ready(function(){
     var android_pv=0;
     var wx_pv=0;
     var unknow_pv=0;
+    var dd_pv=0;
+    var max_pv=0;
     
     data.map(function(one,index,arr){
         var ua=one.ua;
@@ -17,8 +19,23 @@ $(document).ready(function(){
         if(ua.indexOf('MicroMessenger')!=-1){
             wx_pv+=1;
         }
-        unknow_pv=all_pv-ios_pv-android_pv;
+        if(ua.indexOf('DingTalk')!=-1){
+            dd_pv+=1;
+        }
     });
+    
+    unknow_pv=all_pv-ios_pv-android_pv;    
+    if(max_pv<ios_pv){
+        max_pv=ios_pv;
+    }
+    if(max_pv<android_pv){
+        max_pv=android_pv;
+    }
+    if(max_pv<unknow_pv){
+        max_pv=unknow_pv;
+    }
+    
+    console.log(max_pv);
     
     $('.all_pv').text(all_pv);
     $('.ios_pv').text(ios_pv);
@@ -43,124 +60,70 @@ $(document).ready(function(){
                     labelLine: {show:false}
                 }
             };
-            var placeHolderStyle = {
-                normal : {
-                    color: 'rgba(0,0,0,0)',
-                    label: {show:false},
-                    labelLine: {show:false}
-                },
-                emphasis : {
-                    color: 'rgba(0,0,0,0)'
-                }
-            };
+            
+            //
             option = {
-                title: {
-                    text: 'PV 统计',
-                    subtext: '',
-                    sublink: 'http://www.gongls.com',
-                    x: 'center',
-                    y: 'center',
-                    itemGap: 20,
-                    textStyle : {
-                        color : 'rgba(30,144,255,0.8)',
-                        fontFamily : '微软雅黑',
-                        fontSize : 35,
-                        fontWeight : 'bolder'
-                    }
-                },
                 tooltip : {
-                    show: true,
+                    trigger: 'item',
                     formatter: "{a} <br/>{b} : {c} ({d}%)"
-                },
-                legend: {
-                    orient : 'vertical',
-                    x : document.getElementById('main').offsetWidth / 2,
-                    y : 45,
-                    itemGap:4,
-                    data:['ios','安卓','微信','未知']
                 },
                 toolbox: {
                     show : false
                 },
+                calculable : false,
                 series : [
                     {
-                        name:'ios',
+                        name:'访问来源',
                         type:'pie',
-                        clockWise:false,
-                        radius : [125, 150],
-                        itemStyle : dataStyle,
-                        data:[
-                            {
-                                value:ios_pv,
-                                name:'ios_pv'
-                            },
-                            {
-                                value:all_pv-ios_pv,
-                                name:'invisible',
-                                itemStyle : placeHolderStyle
+                        selectedMode: 'single',
+                        radius : [0, 70],
+
+                        // for funnel
+                        x: '20%',
+                        width: '40%',
+                        funnelAlign: 'right',
+                        max: max_pv,
+
+                        itemStyle : {
+                            normal : {
+                                label : {
+                                    position : 'inner'
+                                },
+                                labelLine : {
+                                    show : false
+                                }
                             }
+                        },
+                        data:[
+                            {value:ios_pv, name:'IOS'},
+                            {value:android_pv, name:'安卓'},
+                            {value:unknow_pv, name:'未知'}
                         ]
                     },
                     {
-                        name:'安卓',
+                        name:'访问来源',
                         type:'pie',
-                        clockWise:false,
-                        radius : [100, 125],
-                        itemStyle : dataStyle,
+                        radius : [100, 140],
+
+                        // for funnel
+                        x: '60%',
+                        width: '35%',
+                        funnelAlign: 'left',
+                        max: 0,
+
                         data:[
-                            {
-                                value:android_pv, 
-                                name:'android_pv'
-                            },
-                            {
-                                value:all_pv-android_pv,
-                                name:'invisible',
-                                itemStyle : placeHolderStyle
-                            }
-                        ]
-                    },
-                    {
-                        name:'微信',
-                        type:'pie',
-                        clockWise:false,
-                        radius : [75, 100],
-                        itemStyle : dataStyle,
-                        data:[
-                            {
-                                value:wx_pv, 
-                                name:'wx_pv'
-                            },
-                            {
-                                value:all_pv-wx_pv,
-                                name:'invisible',
-                                itemStyle : placeHolderStyle
-                            }
-                        ]
-                    },
-                    {
-                        name:'未知设备',
-                        type:'pie',
-                        clockWise:false,
-                        radius : [75, 100],
-                        itemStyle : dataStyle,
-                        data:[
-                            {
-                                value:unknow_pv, 
-                                name:'unknow_pv'
-                            },
-                            {
-                                value:all_pv-unknow_pv,
-                                name:'invisible',
-                                itemStyle : placeHolderStyle
-                            }
+                            {value:wx_pv, name:'微信'},
+                            {value:dd_pv, name:'钉钉'}
                         ]
                     }
                 ]
             };
-        
-                // 为echarts对象加载数据 
-                myChart.setOption(option); 
-            }
-        );
+
+                          
+            //
+            // 为echarts对象加载数据 
+            myChart.setOption(option); 
+        }
+    );
     //
 });
