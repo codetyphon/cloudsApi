4e048ef82efc89cce5e92ad5b040512732d9a801
codetyphon <codetyphon@sina.com>
2016-04-08 14:43:43 +0800

    ...
    …

--- public/javascripts/data.js
+++ public/javascripts/data.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
     var all_pv=data.length;
     var ios_pv=0;
     var android_pv=0;
+    var wx_pv=0;
     
     data.map(function(one,index,arr){
         var ua=one.ua;
@@ -11,6 +12,9 @@ $(document).ready(function(){
         }
         if(ua.indexOf('Android')!=-1){
             android_pv+=1;
+        }
+        if(ua.indexOf('MicroMessenger ')!=-1){
+            wx_pv+=1;
         }
     });
     
@@ -31,38 +35,114 @@ $(document).ready(function(){
             function (ec) {
                 // 基于准备好的dom，初始化echarts图表
                 var myChart = ec.init(document.getElementById('main')); 
-                option = {
-                    title : {
-                        text: 'pv统计',
-                        subtext: '热门话题 h5',
-                        x:'center'
+                var dataStyle = {
+                normal: {
+                    label: {show:false},
+                    labelLine: {show:false}
+                }
+            };
+            var placeHolderStyle = {
+                normal : {
+                    color: 'rgba(0,0,0,0)',
+                    label: {show:false},
+                    labelLine: {show:false}
+                },
+                emphasis : {
+                    color: 'rgba(0,0,0,0)'
+                }
+            };
+            option = {
+                title: {
+                    text: 'PV 统计',
+                    subtext: '',
+                    sublink: 'http://www.gongls.com',
+                    x: 'center',
+                    y: 'center',
+                    itemGap: 20,
+                    textStyle : {
+                        color : 'rgba(30,144,255,0.8)',
+                        fontFamily : '微软雅黑',
+                        fontSize : 35,
+                        fontWeight : 'bolder'
+                    }
+                },
+                tooltip : {
+                    show: true,
+                    formatter: "{a} <br/>{b} : {c} ({d}%)"
+                },
+                legend: {
+                    orient : 'vertical',
+                    x : document.getElementById('main').offsetWidth / 2,
+                    y : 45,
+                    itemGap:12,
+                    data:['ios_pv','android_pv','wx_pv']
+                },
+                toolbox: {
+                    show : true,
+                    feature : {
+                        mark : {show: true},
+                        dataView : {show: true, readOnly: false},
+                        restore : {show: true},
+                        saveAsImage : {show: true}
+                    }
+                },
+                series : [
+                    {
+                        name:'1',
+                        type:'pie',
+                        clockWise:false,
+                        radius : [125, 150],
+                        itemStyle : dataStyle,
+                        data:[
+                            {
+                                value:ios_pv,
+                                name:'ios_pv'
+                            },
+                            {
+                                value:all_pv-ios_pv,
+                                name:'invisible',
+                                itemStyle : placeHolderStyle
+                            }
+                        ]
                     },
-                    tooltip : {
-                        trigger: 'item',
-                        formatter: "{a} <br/>{b} : {c} ({d}%)"
+                    {
+                        name:'2',
+                        type:'pie',
+                        clockWise:false,
+                        radius : [100, 125],
+                        itemStyle : dataStyle,
+                        data:[
+                            {
+                                value:android_pv, 
+                                name:'android_pv'
+                            },
+                            {
+                                value:all_pv-android_pv,
+                                name:'invisible',
+                                itemStyle : placeHolderStyle
+                            }
+                        ]
                     },
-                    legend: {
-                        orient : 'vertical',
-                        x : 'left',
-                        data:['ios','android']
-                    },
-                    toolbox: {
-                        show : false
-                    },
-                    calculable : true,
-                    series : [
-                        {
-                            name:'访问来源',
-                            type:'pie',
-                            radius : '55%',
-                            center: ['50%', '60%'],
-                            data:[
-                                {value:ios_pv, name:'ios'},
-                                {value:android_pv, name:'android'}
-                            ]
-                        }
-                    ]
-                };
+                    {
+                        name:'3',
+                        type:'pie',
+                        clockWise:false,
+                        radius : [75, 100],
+                        itemStyle : dataStyle,
+                        data:[
+                            {
+                                value:wx_pv, 
+                                name:'wx_pv'
+                            },
+                            {
+                                value:all_pv-wx_pv,
+                                name:'invisible',
+                                itemStyle : placeHolderStyle
+                            }
+                        ]
+                    }
+                ]
+            };
         
                 // 为echarts对象加载数据 
                 myChart.setOption(option); 
